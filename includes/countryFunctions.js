1ff2a29dabc1e7ea3f6c564707c83c08ccb41c4f
rotem <rotemati1988@gmail.com>
2015-02-22 18:37:21 +0200

    bug fix

--- includes/countryFunctions.js
+++ includes/countryFunctions.js
@@ -10,10 +10,9 @@ $(document).ready(function() {
     	}
 	};
 	$.getJSON("./json/countries.json",function(data) { //insert dynamic data from json
-		var currentCountry = currentId("id");
-		var currentFlagUrl = data.countries[currentCountry-1].flagUrl;
-		if (currentCountry == 1) { //holland page
-			$("main").append("<h1 id='country_h1'>HOLLAND PAGE</h1>"+
+		var currentCountry = data.countries[currentId("id")-1];
+		//var currentFlagUrl = data.countries[currentCountry].flagUrl;
+			$("main").append("<h1 id='country_h1'>"+currentCountry.home+" PAGE</h1>"+
 							"<section id='main_img_country_section'>"+
 								"<p id='main_country_wellcome'>Welcome to Tafnit Program's 2012</p>"+
 								"<img src='images/country_azrieli.jpg' id='country_main_image'>"+
@@ -35,106 +34,9 @@ $(document).ready(function() {
 								"</section>"+
 							"</section>"+
 							"<section id='info_with_flag'>"+
-								"<img src="+currentFlagUrl+" id='country_flag'>"+
-								"<p id='country_description'>"+data.countries[currentCountry-1].description+"</p>"+
-							"</section>"
-							
-							
-			
-			);
-								
-			
-		}
-		else if (currentCountry == 2) { //usa page
-			$("main").append("<h1 id='events_h1'>USA PAGE</h1>"+
-							"<section id='main_img_country_section'>"+
-								"<p id='main_country_wellcome'>Welcome to Tafnit Program's 2012</p>"+
-								"<img src='images/country_azrieli.jpg' id='country_main_image'>"+
-								"<section id='four_element_country'>"+
-									"<img src='images/congress.jpg' id='upper_left'>"+
-									"<section id='upper_right'>"+
-										"<h2 class='h2_four_element'>Welcome to<br> Tafnit Program's 2011</h2>"+
-										"<p id='four_element_info'>"+
-											"worldwide community of Jewish professionals, business leaders and executives who have a strong desire to connect and executives who have a strong to connect and executives"+
-										"</p>"+ 
-									"</section>"+
-									"<section id='lower_left'>"+
-										"<h2 class='h2_four_element'>Welcome to<br> Tafnit Program's 2011</h2>"+
-										"<p id='four_element_info'>"+
-											"worldwide community of Jewish professionals, business leaders and executives who have a strong desire to connect and executives who have a strong to connect and executives"+
-										"</p>"+ 
-									"</section>"+
-									"<img src='images/azrieli_evening.jpg' id='lower_right'>"+
-								"</section>"+
-							"</section>"+
-							"<section id='info_with_flag'>"+
-								"<img src="+currentFlagUrl+" id='country_flag'>"+
-								"<p id='country_description'>"+data.countries[currentCountry-1].description+"</p>"+
-							"</section>"
-			
-			
-			);
-		}
-		else if (currentCountry == 3) { //israel page
-			$("main").append("<h1 id='events_h1'>ISRAEL PAGE</h1>"+
-							"<section id='main_img_country_section'>"+
-								"<p id='main_country_wellcome'>Welcome to Tafnit Program's 2012</p>"+
-								"<img src='images/country_azrieli.jpg' id='country_main_image'>"+
-								"<section id='four_element_country'>"+
-									"<img src='images/congress.jpg' id='upper_left'>"+
-									"<section id='upper_right'>"+
-										"<h2 class='h2_four_element'>Welcome to<br> Tafnit Program's 2011</h2>"+
-										"<p id='four_element_info'>"+
-											"worldwide community of Jewish professionals, business leaders and executives who have a strong desire to connect and executives who have a strong to connect and executives"+
-										"</p>"+ 
-									"</section>"+
-									"<section id='lower_left'>"+
-										"<h2 class='h2_four_element'>Welcome to<br> Tafnit Program's 2011</h2>"+
-										"<p id='four_element_info'>"+
-											"worldwide community of Jewish professionals, business leaders and executives who have a strong desire to connect and executives who have a strong to connect and executives"+
-										"</p>"+ 
-									"</section>"+
-									"<img src='images/azrieli_evening.jpg' id='lower_right'>"+
-								"</section>"+
-							"</section>"+
-							"<section id='info_with_flag'>"+
-								"<img src="+currentFlagUrl+" id='country_flag'>"+
-								"<p id='country_description'>"+data.countries[currentCountry-1].description+"</p>"+
-							"</section>"
-			);
-		}
-		else { //spain page
-			$("main").append("<h1 id='events_h1'>SPAIN PAGE</h1>"+
-							"<section id='main_img_country_section'>"+
-								"<p id='main_country_wellcome'>Welcome to Tafnit Program's 2012</p>"+
-								"<img src='images/country_azrieli.jpg' id='country_main_image'>"+
-								"<section id='four_element_country'>"+
-									"<img src='images/congress.jpg' id='upper_left'>"+
-									"<section id='upper_right'>"+
-										"<h2 class='h2_four_element'>Welcome to<br> Tafnit Program's 2011</h2>"+
-										"<p id='four_element_info'>"+
-											"worldwide community of Jewish professionals, business leaders and executives who have a strong desire to connect and executives who have a strong to connect and executives"+
-										"</p>"+ 
-									"</section>"+
-									"<section id='lower_left'>"+
-										"<h2 class='h2_four_element'>Welcome to<br> Tafnit Program's 2011</h2>"+
-										"<p id='four_element_info'>"+
-											"worldwide community of Jewish professionals, business leaders and executives who have a strong desire to connect and executives who have a strong to connect and executives"+
-										"</p>"+ 
-									"</section>"+
-									"<img src='images/azrieli_evening.jpg' id='lower_right'>"+
-								"</section>"+
-							"</section>"+
-							"<section id='info_with_flag'>"+
-								"<img src="+currentFlagUrl+" id='country_flag'>"+
-								"<p id='country_description'>"+data.countries[currentCountry-1].description+"</p>"+
-							"</section>"
-			
-			
-			
-			);
-		}
-
-
+								"<img src="+currentCountry.flagUrl+" id='country_flag'>"+
+								"<p id='country_description'>"+currentCountry.description+"</p>"+
+							"</section>" );
 	});
+	
 });
