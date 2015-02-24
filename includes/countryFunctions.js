09e507e9fef315d69bd42a4b03da66ac3edb8189
benmarder <benmarder@live.com>
2015-02-24 19:42:03 +0200

    more efficient parse from url  + text alignment

--- includes/countryFunctions.js
+++ includes/countryFunctions.js
@@ -1,16 +1,6 @@
 $(document).ready(function() {
-	var currentId = function getUrlParameter(sParam) {
-    	var sPageURL = window.location.search.substring(1);
-    	var sURLVariables = sPageURL.split('&');
-    	for (var i = 0; i < sURLVariables.length; i++) {
-        	var sParameterName = sURLVariables[i].split('=');
-        	if (sParameterName[0] == sParam) {
-        	   	return sParameterName[1];
-        	}
-    	}
-	};
 	$.getJSON("./json/countries.json",function(data) { //insert dynamic data from json
-		var currentCountry = data.countries[currentId("id")-1];
+		var currentCountry = data.countries[window.location.search.match(/\d+/)-1];
 		//var currentFlagUrl = data.countries[currentCountry].flagUrl;
 			$("main").append("<h1 id='country_h1'>"+currentCountry.home+" PAGE</h1>"+
 							"<section id='main_img_country_section'>"+
@@ -21,7 +11,7 @@ $(document).ready(function() {
 									"<section id='upper_right'>"+
 										"<h2 class='h2_four_element'>Welcome to<br> Tafnit Program's 2011</h2>"+
 										"<p id='four_element_info'>"+
-											"worldwide community of Jewish professionals, business leaders and executives who have a strong desire to connect and executives who have a strong to connect and executives"+
+											"worldwide community of Jewish professionals, business leaders and executives who have a strong desire<br>to connect and executives who have<br>a strong to connect and executives<br>"+
 										"</p>"+ 
 									"</section>"+
 									"<section id='lower_left'>"+
